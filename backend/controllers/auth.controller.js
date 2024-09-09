5f6321ee13de694d7eaa13458d4bc7442b624a60
Jignesh <jigneshmal.com>
2024-09-09 16:41:54 +0530

    login endpoint created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -53,7 +53,39 @@ export const SignUp = async (req, res) => {
 };
 
 export const Login = async (req, res) => {
-  res.send("Login route");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: null,
+      },
+    });
+  } catch (error) {
+    console.log("error in login", error);
+
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 export const Logout = async (req, res) => {
